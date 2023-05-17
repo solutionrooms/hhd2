dfcb41f49676168769cf595738600cdf67a0a8b8
Jon Scott <jon.scott@solutionrooms.com>
2023-05-17 11:49:05 +0100

    initial

--- patients/forms.py
+++ patients/forms.py
@@ -3,6 +3,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.utils import timezone
 
+from crispy_forms.layout import Layout, Field
 
 from .models import Measurement
 
@@ -13,12 +14,17 @@ class MeasurementForm(forms.ModelForm):
 
     class Meta:
         model = Measurement
-        fields = ['patient', 'weight_grams', 'notes']
+        fields = ['patient', 'measurement_timestamp','weight_grams', 'notes']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Field('measurement_timestamp', css_class='col-12 col-sm-6'),
+            Field('weight_grams', css_class='col-12 col-sm-6'),
+            Field('notes', css_class='col-12'),
+        )
         self.helper.add_input(Submit('submit', 'Save measurement'))
 
 
