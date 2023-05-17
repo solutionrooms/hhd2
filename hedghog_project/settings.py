311a7cadb2da18edd8bf215862837afdc5bfc2d6
Jon Scott <jon.scott@solutionrooms.com>
2023-05-17 22:38:15 +0100

    initial

--- hedghog_project/settings.py
+++ hedghog_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-#3ucv7m^y05dkmdqd+g!kj+lf-@m7z2_6@bz3s97k@r=-du+ob
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
